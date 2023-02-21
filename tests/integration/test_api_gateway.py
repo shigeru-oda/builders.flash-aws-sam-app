1ffdef8fbcaf46c741b259613939ccb03564d0ed
ShigeruOda <seq.drug@gmail.com>
2023-02-21 20:26:25 +0900

    version2

--- tests/integration/test_api_gateway.py
+++ tests/integration/test_api_gateway.py
@@ -44,3 +44,33 @@ class TestApiGateway:
 
         assert response.status_code == 200
         assert response.json() == {"message": "Hello SAM World"}
+
+    def test_get_item(self, api_gateway_url):
+        """ Call the API Gateway endpoint and check the response """
+        response = requests.get(api_gateway_url + "/item?id=ABC")
+
+        assert response.status_code == 200
+        assert response.json() == {"id":"ABC","name":"get_item+ABC","description":"get_item+ABC","price":100.0}
+
+    def test_post_item(self, api_gateway_url):
+        """ Call the API Gateway endpoint and check the response """
+        request_url = api_gateway_url + "/item"
+        headers = {"Content-Type" : "application/json"}
+        json_data = {"name": "post_item","description":"post_item","price":200.0}
+        response = requests.post(
+            request_url,
+            headers=headers,
+            json=json_data
+        )
+        del_id_res = response.json()
+        print(del_id_res)
+        del del_id_res["id"]
+        assert response.status_code == 201
+        assert del_id_res =={"name": "post_item","description":"post_item","price":200.0}
+
+    def test_put_item(self, api_gateway_url):
+        """ Call the API Gateway endpoint and check the response """
+        response = requests.put(api_gateway_url + "/item/9")
+
+        assert response.status_code == 201
+        assert response.json() =={"id": "9", "name": "put_item+9","description":"put_item+9","price":300.0}
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,9 +1,34 @@
 from fastapi.testclient import TestClient
-from hello_world.app import app
+from hello_world.app import app as hello_world_app
+from item.app import app as item_app
+import json
 
-client = TestClient(app)
+hello_world_client = TestClient(hello_world_app)
+item_client = TestClient(item_app)
 
 def test_root():
-    response = client.get("/hello")
+    response = hello_world_client.get("/hello")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello SAM World"}
+
+def test_get_item():
+    response = item_client.get("/item?id=ABC")
+    assert response.status_code == 200
+    assert response.json() == {"id": "ABC", "name": "get_item+ABC","description":"get_item+ABC","price":100.0}
+
+def test_post_item():
+    response = item_client.post(
+        "/item",
+        json={"name": "post_item","description":"post_item","price":200.0}
+    )
+    del_id_res = response.json()
+    del del_id_res["id"]
+    assert response.status_code == 201
+    assert del_id_res =={"name": "post_item","description":"post_item","price":200.0}
+
+def test_put_item():
+    response = item_client.put(
+        "/item/9"
+    )
+    assert response.status_code == 201
+    assert response.json() =={"id": "9", "name": "put_item+9","description":"put_item+9","price":300.0}
